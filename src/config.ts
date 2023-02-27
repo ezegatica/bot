5734f2eaea0a46291c27876c69fa58186ecbbf9b
Eze Gatica <ezequielgatica@gmail.com>
2023-02-26 23:07:00 -0300

    Arreglando puerto default

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   NODE_ENV: process.env.NODE_ENV || 'production',
   DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT || 8080
+  PORT: process.env.PORT || 80
 };
 
 export default config;
